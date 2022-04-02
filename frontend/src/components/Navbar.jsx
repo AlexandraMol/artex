0d311de90dda1a1407123e54a6a24ba77e55fb29
AlexandraMol <56740859+AlexandraMol@users.noreply.github.com>
2022-04-02 20:42:27 +0300

    finished navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
 import "../assets/css/Navbar.css";
+import { FaSearch, FaUser, FaSignOutAlt } from "react-icons/fa";
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="Navbar">
-      <span className="nav-logo">DevLHB</span>
+      <span className="nav-logo">
+        Artex
+        <FaSearch />
+      </span>
       <div className={`nav-items ${isOpen && "open"}`}>
-        <a href="/home">Home</a>
-        <a href="/about">About</a>
-        <a href="/service">Service</a>
-        <a href="/contact">Contact</a>
+        <a href="/home">Article Analysis</a>
+        <a href="/about">View Articles</a>
+        <a href="/service">My Articles</a>
+        <a href="/contact">Profile</a>
+        <a href="/logout">Logout</a>
       </div>
       <div
         className={`nav-toggle ${isOpen && "open"}`}
