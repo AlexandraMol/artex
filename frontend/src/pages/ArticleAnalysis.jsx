9393eafa3758ab1bf84ad05267450daf2101c225
AlexandraMol <56740859+AlexandraMol@users.noreply.github.com>
2022-04-13 15:55:07 +0300

    added analyzer final version

--- frontend/src/pages/ArticleAnalysis.jsx
+++ frontend/src/pages/ArticleAnalysis.jsx
@@ -5,17 +5,25 @@ import { Button } from "primereact/button";
 import { InputText } from "primereact/inputtext";
 import { InputTextarea } from "primereact/inputtextarea";
 import { MultiSelect } from "primereact/multiselect";
-
+import { useNavigate } from "react-router";
 const ArticleAnalysis = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
-
+  const navigate = useNavigate();
   const [selectedAnalysis, setAnalysis] = useState(null);
 
   const analysis = [
     { name: "LDA", code: "LDA" },
     { name: "Sentiment Analysis", code: "SA" },
   ];
+
+  function cancelFunction() {
+    navigate("/articles");
+  }
+
+  function submitFunction() {
+    // TODO importat modala si generat raportul plus adaugat functionalitatea de download as PDF
+  }
 
   return (
     <>
@@ -27,6 +35,7 @@ const ArticleAnalysis = () => {
               <div className={styles.titleAndType}>
                 <span className="p-float-label">
                   <InputText
+                    style={{ width: "50vh" }}
                     id="in"
                     onChange={(e) => {
                       setTitle(e.target.value);
@@ -47,21 +56,28 @@ const ArticleAnalysis = () => {
                   />
                 </div>
               </div>
+
+              <span className="p-float-label">
+                <InputTextarea
+                  style={{ width: "80vh", height: "30vh", resize: "none" }}
+                  id="out"
+                  onChange={(e) => {
+                    setContent(e.target.value);
+                  }}
+                  className="p-inputtextarea"
+                />
+                <label htmlFor="in">Article content</label>
+              </span>
+
+              <div className={styles.buttonsContainer}>
+                <Button
+                  label="Cancel"
+                  style={{ backgroundColor: "gray" }}
+                  onClick={cancelFunction}
+                />
+                <Button label="Submit" onClick={submitFunction} />
+              </div>
             </div>
-
-            <span className="p-float-label">
-              <InputTextarea
-                id="out"
-                onChange={(e) => {
-                  setContent(e.target.value);
-                }}
-                className="p-inputtextarea"
-              />
-              <label htmlFor="in">Article content</label>
-            </span>
-
-            <Button label="Cancel" />
-            <Button label="Submit" />
           </div>
         </div>
       </div>
